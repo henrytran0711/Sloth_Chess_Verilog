73d5103893d7e2ec23109d827d9fec4dc006cf6e
henrytran0711 <35233855+henrytran0711@users.noreply.github.com>
2018-04-28 00:37:22 -0700

    Done

--- arbiter_passthrough.v
+++ arbiter_passthrough.v
@@ -1,18 +1,20 @@
-module Arb (in1,in2, out);
+module Arb (in1,in2,inpos1,inpos2, out,outpos);
 input [5:0] in1;
 input [5:0] in2;
 input[5:0] inpos1;
 input[5:0] inpos2;
 output reg [5:0] out;
-output reg[5:0] outpos;
+output reg [5:0] outpos;
   always @(*) 
     begin
-if (in1 > in2)
+if (in1 > in2) begin
 	out = in1;
 	outpos = inpos1;
-else if (in2 >= in1)
+	end
+else if (in2 >= in1) begin
 	out = in2;
 	outpos = inpos2;
+	end
  	 end
 endmodule 
 
@@ -303,85 +305,85 @@ wire [5:0] out2pos_1;
 wire [5:0] out2pos_2;
 //Out 1
 output [5:0] out;
-output [5:0] outpos;
+output [5:0] out_pos;
 //32-Instantiation Level:
 	Arb Arb32_1 (
 		.in1 (in1),
 		.in2 (in2),
-		.inpos1 (inpos_1)
-		.inpos2(inpos_2)
+		.inpos1 (inpos_1),
+		.inpos2(inpos_2),
 		.out(out32_1),
 		.outpos(out32pos_1)
 		);
 	Arb Arb32_2 (
 		.in1 (in3),
 		.in2 (in4),
-		.inpos1 (inpos_3)
-		.inpos2(inpos_4)
+		.inpos1 (inpos_3),
+		.inpos2(inpos_4),
 		.out(out32_2),
 		.outpos(out32pos_2)
 		);
 	Arb Arb32_3 (
 		.in1 (in5),
 		.in2 (in6),
-		.inpos1 (inpos_5)
-		.inpos2(inpos_6)
+		.inpos1 (inpos_5),
+		.inpos2(inpos_6),
 		.out(out32_3),
 		.outpos(out32pos_3)
 		);
 	Arb Arb32_4 (
 		.in1 (in7),
 		.in2 (in8),
-		.inpos1 (inpos_7)
-		.inpos2(inpos_8)
+		.inpos1 (inpos_7),
+		.inpos2(inpos_8),
 		.out(out32_4),
 		.outpos(out32pos_4)
 		);
 	Arb Arb32_5 (
 		.in1 (in9),
 		.in2 (in10),
-		.inpos1 (inpos_9)
-		.inpos2(inpos_10)
+		.inpos1 (inpos_9),
+		.inpos2(inpos_10),
 		.out(out32_5),
 		.outpos(out32pos_5)
 		);
 	Arb Arb32_6 (
 		.in1 (in11),
 		.in2 (in12),
-		.inpos1 (inpos_11)
-		.inpos2(inpos_12)
+		.inpos1 (inpos_11),
+		.inpos2(inpos_12),
 		.out(out32_6),
 		.outpos(out32pos_6)
 		);
 	Arb Arb32_7 (
 		.in1 (in13),
 		.in2 (in14),
-		.inpos1 (inpos_13)
-		.inpos2(inpos_14)
+		.inpos1 (inpos_13),
+		.inpos2(inpos_14),
 		.out(out32_7),
 		.outpos(out32pos_7)
 		);
 	Arb Arb32_8 (
 		.in1 (in15),
 		.in2 (in16),
-		.inpos1 (inpos_15)
-		.inpos2(inpos_16)
+		.inpos1 (inpos_15),
+		.inpos2(inpos_16),
 		.out(out32_8),
 		.outpos(out32pos_8)
 		);
 	Arb Arb32_9 (
 		.in1 (in17),
 		.in2 (in18),
-		.inpos1 (inpos_17)
-		.inpos2(inpos_18)
+		.inpos1 (inpos_17),
+		.inpos2(inpos_18),
 		.out(out32_9),
 		.outpos(out32pos_9)
 		);
 	Arb Arb32_10 (
 		.in1 (in19),
 		.in2 (in20),
-		.inpos1 (inpos_19)
-		.inpos2(inpos_20)
+		.inpos1 (inpos_19),
+		.inpos2(inpos_20),
 		.out(out32_10),
 		.outpos(out32pos_10)
 		);
@@ -389,8 +391,8 @@ output [5:0] outpos;
 	Arb Arb32_11 (
 		.in1 (in21),
 		.in2 (in22),
-		.inpos1 (inpos_21)
-		.inpos2(inpos_22)
+		.inpos1 (inpos_21),
+		.inpos2(inpos_22),
 		.out(out32_11),
 		.outpos(out32pos_11)
 		);
@@ -398,168 +400,168 @@ output [5:0] outpos;
 	Arb Arb32_12 (
 		.in1 (in23),
 		.in2 (in24),
-		.inpos1 (inpos_23)
-		.inpos2(inpos_24)
+		.inpos1 (inpos_23),
+		.inpos2(inpos_24),
 		.out(out32_12),
 		.outpos(out32pos_12)
 		);
 	Arb Arb32_13 (
 		.in1 (in25),
 		.in2 (in26),
-		.inpos1 (inpos_25)
-		.inpos2(inpos_26)
+		.inpos1 (inpos_25),
+		.inpos2(inpos_26),
 		.out(out32_13),
 		.outpos(out32pos_13)
 		);
 	Arb Arb32_14 (
 		.in1 (in27),
 		.in2 (in28),
-		.inpos1 (inpos_27)
-		.inpos2(inpos_28)
+		.inpos1 (inpos_27),
+		.inpos2(inpos_28),
 		.out(out32_14),
 		.outpos(out32pos_14)
 		);
 	Arb Arb32_15 (
 		.in1 (in29),
 		.in2 (in30),
-		.inpos1 (inpos_29)
-		.inpos2(inpos_30)
+		.inpos1 (inpos_29),
+		.inpos2(inpos_30),
 		.out(out32_15),
 		.outpos(out32pos_15)
 		);
 	Arb Arb32_16 (
 		.in1 (in31),
 		.in2 (in32),
-		.inpos1 (inpos_31)
-		.inpos2(inpos_32)
+		.inpos1 (inpos_31),
+		.inpos2(inpos_32),
 		.out(out32_16),
 		.outpos(out32pos_16)
 		);
 	Arb Arb32_17 (
 		.in1 (in33),
 		.in2 (in34),
-		.inpos1 (inpos_33)
-		.inpos2(inpos_34)
+		.inpos1 (inpos_33),
+		.inpos2(inpos_34),
 		.out(out32_17),
 		.outpos(out32pos_17)
 		);
 	Arb Arb32_18 (
 		.in1 (in35),
 		.in2 (in36),
-		.inpos1 (inpos_35)
-		.inpos2(inpos_36)
+		.inpos1 (inpos_35),
+		.inpos2(inpos_36),
 		.out(out32_18),
 		.outpos(out32pos_18)
 		);
 	Arb Arb32_19 (
 		.in1 (in37),
 		.in2 (in38),
-		.inpos1 (inpos_37)
-		.inpos2(inpos_38)
+		.inpos1 (inpos_37),
+		.inpos2(inpos_38),
 		.out(out32_19),
 		.outpos(out32pos_19)
 		);
 	Arb Arb32_20 (
 		.in1 (in39),
 		.in2 (in40),
-		.inpos1 (inpos_39)
-		.inpos2(inpos_40)
+		.inpos1 (inpos_39),
+		.inpos2(inpos_40),
 		.out(out32_20),
 		.outpos(out32pos_20)
 		);
 	Arb Arb32_21 (
 		.in1 (in41),
 		.in2 (in42),
-		.inpos1 (inpos_41)
-		.inpos2(inpos_42)
+		.inpos1 (inpos_41),
+		.inpos2(inpos_42),
 		.out(out32_21),
 		.outpos(out32pos_21)
 		);
 	Arb Arb32_22 (
 		.in1 (in43),
 		.in2 (in44),
-		.inpos1 (inpos_43)
-		.inpos2(inpos_44)
+		.inpos1 (inpos_43),
+		.inpos2(inpos_44),
 		.out(out32_22),
 		.outpos(out32pos_22)
 		);
 	Arb Arb32_23 (
 		.in1 (in45),
 		.in2 (in46),
-		.inpos1 (inpos_45)
-		.inpos2(inpos_46)
+		.inpos1 (inpos_45),
+		.inpos2(inpos_46),
 		.out(out32_23),
 		.outpos(out32pos_23)
 		);
 	Arb Arb32_24 (
 		.in1 (in47),
 		.in2 (in48),
-		.inpos1 (inpos_47)
-		.inpos2(inpos_48)
+		.inpos1 (inpos_47),
+		.inpos2(inpos_48),
 		.out(out32_24),
 		.outpos(out32pos_24)
 		);
 	Arb Arb32_25 (
 		.in1 (in49),
 		.in2 (in50),
-		.inpos1 (inpos_49)
-		.inpos2(inpos_50)
+		.inpos1 (inpos_49),
+		.inpos2(inpos_50),
 		.out(out32_25),
 		.outpos(out32pos_25)
 		);
 	Arb Arb32_26 (
 		.in1 (in51),
 		.in2 (in52),
-		.inpos1 (inpos_51)
-		.inpos2(inpos_52)
+		.inpos1 (inpos_51),
+		.inpos2(inpos_52),
 		.out(out32_26),
 		.outpos(out32pos_26)
 		);
 	Arb Arb32_27 (
 		.in1 (in53),
 		.in2 (in54),
-		.inpos1 (inpos_53)
-		.inpos2(inpos_54)
+		.inpos1 (inpos_53),
+		.inpos2(inpos_54),
 		.out(out32_27),
 		.outpos(out32pos_27)
 		);
 	Arb Arb32_28 (
 		.in1 (in55),
 		.in2 (in56),
-		.inpos1 (inpos_55)
-		.inpos2(inpos_56)
+		.inpos1 (inpos_55),
+		.inpos2(inpos_56),
 		.out(out32_28),
 		.outpos(out32pos_28)
 		);
 	Arb Arb32_29 (
 		.in1 (in57),
 		.in2 (in58),
-		.inpos1 (inpos_57)
-		.inpos2(inpos_58)
+		.inpos1 (inpos_57),
+		.inpos2(inpos_58),
 		.out(out32_29),
 		.outpos(out32pos_29)
 		);	
 	Arb Arb32_30 (
 		.in1 (in59),
 		.in2 (in60),
-		.inpos1 (inpos_59)
-		.inpos2(inpos_60)
+		.inpos1 (inpos_59),
+		.inpos2(inpos_60),
 		.out(out32_30),
 		.outpos(out32pos_30)
 		);
 	Arb Arb32_31 (
 		.in1 (in61),
 		.in2 (in62),
-		.inpos1 (inpos_61)
-		.inpos2(inpos_62)
+		.inpos1 (inpos_61),
+		.inpos2(inpos_62),
 		.out(out32_31),
 		.outpos(out32pos_31)
 		);
 	Arb Arb32_32 (
 		.in1 (in63),
 		.in2 (in64),
-		.inpos1 (inpos_63)
-		.inpos2(inpos_64)
+		.inpos1 (inpos_63),
+		.inpos2(inpos_64),
 		.out(out32_32),
 		.outpos(out32pos_32)
 		);
@@ -568,128 +570,128 @@ output [5:0] outpos;
 Arb Arb16_1 (
 		.in1 (out32_1),
 		.in2 (out32_2),
-		.inpos1 (out32pos_1)
-		.inpos2 (out32pos_2)
+		.inpos1 (out32pos_1),
+		.inpos2 (out32pos_2),
 		.out(out16_1),
 		.outpos(out16pos_1)
 		);
 Arb Arb16_2 (
 		.in1 (out32_3),
 		.in2 (out32_4),
-		.inpos1 (out32pos_3)
-		.inpos2 (out32pos_4)
+		.inpos1 (out32pos_3),
+		.inpos2 (out32pos_4),
 		.out(out16_2),
 		.outpos(out16pos_2)
 		);
 Arb Arb16_3 (
 		.in1 (out32_5),
 		.in2 (out32_6),
-		.inpos1 (out32pos_5)
-		.inpos2 (out32pos_6)
+		.inpos1 (out32pos_5),
+		.inpos2 (out32pos_6),
 		.out(out16_3),
 		.outpos(out16pos_3)
 		);
 Arb Arb16_4 (
 		.in1 (out32_7),
 		.in2 (out32_8),
-		.inpos1 (out32pos_7)
-		.inpos2 (out32pos_8)
+		.inpos1 (out32pos_7),
+		.inpos2 (out32pos_8),
 		.out(out16_4),
 		.outpos(out16pos_4)
 		);
 Arb Arb16_5 (
 		.in1 (out32_9),
 		.in2 (out32_10),
-		.inpos1 (out32pos_9)
-		.inpos2 (out32pos_10)
+		.inpos1 (out32pos_9),
+		.inpos2 (out32pos_10),
 		.out(out16_5),
 		.outpos(out16pos_5)
 		);
 Arb Arb16_6 (
 		.in1 (out32_11),
 		.in2 (out32_12),
-		.inpos1 (out32pos_11)
-		.inpos2 (out32pos_12)
+		.inpos1 (out32pos_11),
+		.inpos2 (out32pos_12),
 		.out(out16_6),
 		.outpos(out16pos_6)
 		);
 Arb Arb16_7 (
 		.in1 (out32_13),
 		.in2 (out32_14),
-		.inpos1 (out32pos_13)
-		.inpos2 (out32pos_14)
+		.inpos1 (out32pos_13),
+		.inpos2 (out32pos_14),
 		.out(out16_7),
 		.outpos(out16pos_7)
 		);		
 Arb Arb16_8 (
 		.in1 (out32_15),
 		.in2 (out32_16),
-		.inpos1 (out32pos_15)
-		.inpos2 (out32pos_16)
+		.inpos1 (out32pos_15),
+		.inpos2 (out32pos_16),
 		.out(out16_8),
 		.outpos(out16pos_8)
 		);		
 Arb Arb16_9 (
 		.in1 (out32_17),
 		.in2 (out32_18),
-		.inpos1 (out32pos_17)
-		.inpos2 (out32pos_18)
+		.inpos1 (out32pos_17),
+		.inpos2 (out32pos_18),
 		.out(out16_9),
 		.outpos(out16pos_9)
 		);
 Arb Arb16_10 (
 		.in1 (out32_19),
 		.in2 (out32_20),
-		.inpos1 (out32pos_19)
-		.inpos2 (out32pos_20)
+		.inpos1 (out32pos_19),
+		.inpos2 (out32pos_20),
 		.out(out16_10),
 		.outpos(out16pos_10)
 		);
 Arb Arb16_11 (
 		.in1 (out32_21),
 		.in2 (out32_22),
-		.inpos1 (out32pos_21)
-		.inpos2 (out32pos_22)
+		.inpos1 (out32pos_21),
+		.inpos2 (out32pos_22),
 		.out(out16_11),
 		.outpos(out16pos_11)
 		);
 Arb Arb16_12 (
 		.in1 (out32_23),
 		.in2 (out32_24),
-		.inpos1 (out32pos_23)
-		.inpos2 (out32pos_24)
+		.inpos1 (out32pos_23),
+		.inpos2 (out32pos_24),
 		.out(out16_12),
 		.outpos(out16pos_12)
 		);
 Arb Arb16_13 (
 		.in1 (out32_25),
 		.in2 (out32_26),
-		.inpos1 (out32pos_25)
-		.inpos2 (out32pos_26)
+		.inpos1 (out32pos_25),
+		.inpos2 (out32pos_26),
 		.out(out16_13),
 		.outpos(out16pos_13)
 		);
 Arb Arb16_14 (
 		.in1 (out32_27),
 		.in2 (out32_28),
-		.inpos1 (out32pos_27)
-		.inpos2 (out32pos_28)
+		.inpos1 (out32pos_27),
+		.inpos2 (out32pos_28),
 		.out(out16_14),
 		.outpos(out16pos_14)
 		);
 Arb Arb16_15 (
 		.in1 (out32_29),
 		.in2 (out32_30),
-		.inpos1 (out32pos_29)
-		.inpos2 (out32pos_30)
+		.inpos1 (out32pos_29),
+		.inpos2 (out32pos_30),
 		.out(out16_15),
 		.outpos(out16pos_15)
 		);
 Arb Arb16_16 (
 		.in1 (out32_31),
 		.in2 (out32_32),
-		.inpos1 (out32pos_31)
-		.inpos2 (out32pos_32)
+		.inpos1 (out32pos_31),
+		.inpos2 (out32pos_32),
 		.out(out16_16),
 		.outpos(out16pos_16)
 		);
@@ -697,64 +699,64 @@ Arb Arb16_16 (
 Arb Arb8_1 (
 		.in1 (out16_1),
 		.in2 (out16_2),
-		.inpos1 (out16pos_1)
-		.inpos2 (out16pos_2)
+		.inpos1 (out16pos_1),
+		.inpos2 (out16pos_2),
 		.out(out8_1),
 		.outpos(out8pos_1)
 		);
 Arb Arb8_2 (
 		.in1 (out16_3),
 		.in2 (out16_4),
-		.inpos1 (out16pos_3)
-		.inpos2 (out16pos_4)
-		.out(out8_2,
+		.inpos1 (out16pos_3),
+		.inpos2 (out16pos_4),
+		.out(out8_2),
 		.outpos(out8pos_2)
 		);
 Arb Arb8_3 (
 		.in1 (out16_5),
 		.in2 (out16_6),
-		.inpos1 (out16pos_5)
-		.inpos2 (out16pos_6)
+		.inpos1 (out16pos_5),
+		.inpos2 (out16pos_6),
 		.out(out8_3),
 		.outpos(out8pos_3)
 		);
 Arb Arb8_4 (
 		.in1 (out16_7),
 		.in2 (out16_8),
-		.inpos1 (out16pos_7)
-		.inpos2 (out16pos_8)
+		.inpos1 (out16pos_7),
+		.inpos2 (out16pos_8),
 		.out(out8_4),
 		.outpos(out8pos_4)
 		);
 Arb Arb8_5 (
 		.in1 (out16_9),
 		.in2 (out16_10),
-		.inpos1 (out16pos_9)
-		.inpos2 (out16pos_10)
+		.inpos1 (out16pos_9),
+		.inpos2 (out16pos_10),
 		.out(out8_5),
 		.outpos(out8pos_5)
 		);
 Arb Arb8_6 (
 		.in1 (out16_11),
 		.in2 (out16_12),
-		.inpos1 (out16pos_11)
-		.inpos2 (out16pos_12)
+		.inpos1 (out16pos_11),
+		.inpos2 (out16pos_12),
 		.out(out8_6),
 		.outpos(out8pos_6)
 		);
 Arb Arb8_7 (
 		.in1 (out16_13),
 		.in2 (out16_14),
-		.inpos1 (out16pos_13)
-		.inpos2 (out16pos_14)
+		.inpos1 (out16pos_13),
+		.inpos2 (out16pos_14),
 		.out(out8_7),
 		.outpos(out8pos_7)
 		);
 Arb Arb8_8 (
 		.in1 (out16_15),
 		.in2 (out16_16),
-		.inpos1 (out16pos_15)
-		.inpos2 (out16pos_16)
+		.inpos1 (out16pos_15),
+		.inpos2 (out16pos_16),
 		.out(out8_8),
 		.outpos(out8pos_8)
 		);
@@ -762,32 +764,32 @@ Arb Arb8_8 (
 Arb Arb4_1 (
 		.in1 (out8_1),
 		.in2 (out8_2),
-		.inpos1 (out8pos_1)
-		.inpos2 (out8pos_2)
+		.inpos1 (out8pos_1),
+		.inpos2 (out8pos_2),
 		.out(out4_1),
 		.outpos(out4pos_1)
 		);
 Arb Arb4_2 (
 		.in1 (out8_3),
 		.in2 (out8_4),
-		.inpos1 (out8pos_3)
-		.inpos2 (out8pos_4)
+		.inpos1 (out8pos_3),
+		.inpos2 (out8pos_4),
 		.out(out4_2),
 		.outpos(out4pos_2)
 		);
 Arb Arb4_3 (
 		.in1 (out8_5),
 		.in2 (out8_6),
-		.inpos1 (out8pos_5)
-		.inpos2 (out8pos_6)
+		.inpos1 (out8pos_5),
+		.inpos2 (out8pos_6),
 		.out(out4_3),
 		.outpos(out4pos_3)
 		);
 Arb Arb4_4 (
 		.in1 (out8_7),
 		.in2 (out8_8),
-		.inpos1 (out8pos_7)
-		.inpos2 (out8pos_8)
+		.inpos1 (out8pos_7),
+		.inpos2 (out8pos_8),
 		.out(out4_4),
 		.outpos(out4pos_4)
 		);		
@@ -795,25 +797,29 @@ Arb Arb4_4 (
 Arb Arb2_1 (
 		.in1 (out4_1),
 		.in2 (out4_2),
-		.inpos1 (out4pos_1)
-		.inpos2 (out4pos_2)
+		.inpos1 (out4pos_1),
+		.inpos2 (out4pos_2),
 		.out(out2_1),
 		.outpos(out2pos_1)
 		);
 Arb Arb2_2 (
 		.in1 (out4_3),
 		.in2 (out4_4),
-		.inpos1 (out4pos_3)
-		.inpos2 (out4pos_4)
+		.inpos1 (out4pos_3),
+		.inpos2 (out4pos_4),
 		.out(out2_2),
-		.outpos(out2pos_1)
+		.outpos(out2pos_2)
 		);
 
 //Arbiter Level 1
 Arb Arb1_1 (
 		.in1 (out2_1),
 		.in2 (out2_2),
-		.out(out)
+		.inpos1 (out2pos_1),
+		.inpos2 (out2pos_2),
+		.out(out),
+		.outpos(out_pos)
 		);
+
 
 endmodule 
--- tbarbiter_passthrough.v
+++ tbarbiter_passthrough.v
@@ -63,8 +63,73 @@ module tb_Arb_complete;
 	reg [5:0] test_in62;
 	reg [5:0] test_in63;
 	reg [5:0] test_in64;
+	//Pos in 
+	reg [5:0] test_inpos1;
+	reg [5:0] test_inpos2;
+	reg [5:0] test_inpos3;
+	reg [5:0] test_inpos4;
+	reg [5:0] test_inpos5;
+	reg [5:0] test_inpos6;
+	reg [5:0] test_inpos7;
+	reg [5:0] test_inpos8;
+	reg [5:0] test_inpos9;
+	reg [5:0] test_inpos10;
+	reg [5:0] test_inpos11;
+	reg [5:0] test_inpos12;
+	reg [5:0] test_inpos13;
+	reg [5:0] test_inpos14;
+	reg [5:0] test_inpos15;
+	reg [5:0] test_inpos16;
+	reg [5:0] test_inpos17;
+	reg [5:0] test_inpos18;
+	reg [5:0] test_inpos19;
+	reg [5:0] test_inpos20;
+	reg [5:0] test_inpos21;
+	reg [5:0] test_inpos22;
+	reg [5:0] test_inpos23;
+	reg [5:0] test_inpos24;
+	reg [5:0] test_inpos25;
+	reg [5:0] test_inpos26;
+	reg [5:0] test_inpos27;
+	reg [5:0] test_inpos28;
+	reg [5:0] test_inpos29;
+	reg [5:0] test_inpos30;
+	reg [5:0] test_inpos31;
+	reg [5:0] test_inpos32;
+	reg [5:0] test_inpos33;
+	reg [5:0] test_inpos34;
+	reg [5:0] test_inpos35;
+	reg [5:0] test_inpos36;
+	reg [5:0] test_inpos37;
+	reg [5:0] test_inpos38;
+	reg [5:0] test_inpos39;
+	reg [5:0] test_inpos40;
+	reg [5:0] test_inpos41;
+	reg [5:0] test_inpos42;
+	reg [5:0] test_inpos43;
+	reg [5:0] test_inpos44;
+	reg [5:0] test_inpos45;
+	reg [5:0] test_inpos46;
+	reg [5:0] test_inpos47;
+	reg [5:0] test_inpos48;
+	reg [5:0] test_inpos49;
+	reg [5:0] test_inpos50;
+	reg [5:0] test_inpos51;
+	reg [5:0] test_inpos52;
+	reg [5:0] test_inpos53;
+	reg [5:0] test_inpos54;
+	reg [5:0] test_inpos55;
+	reg [5:0] test_inpos56;
+	reg [5:0] test_inpos57;
+	reg [5:0] test_inpos58;
+	reg [5:0] test_inpos59;
+	reg [5:0] test_inpos60;
+	reg [5:0] test_inpos61;
+	reg [5:0] test_inpos62;
+	reg [5:0] test_inpos63;
+	reg [5:0] test_inpos64;
 	wire [5:0] test_out;
-	
+	wire [5:0] test_outpos;
 	
 	Arb_complete Arb_comp (//Inputs
 						.in1(test_in1),
@@ -131,8 +196,74 @@ module tb_Arb_complete;
 						.in62(test_in62),
 						.in63(test_in63),
 						.in64(test_in64),
+						//Input Test-in
+						.inpos_1(test_inpos1),
+						.inpos_2(test_inpos2),
+						.inpos_3(test_inpos3),
+						.inpos_4(test_inpos4),
+						.inpos_5(test_inpos5),
+						.inpos_6(test_inpos6),
+						.inpos_7(test_inpos7),
+						.inpos_8(test_inpos8),
+						.inpos_9(test_inpos9),
+						.inpos_10(test_inpos10),
+						.inpos_11(test_inpos11),
+						.inpos_12(test_inpos12),
+						.inpos_13(test_inpos13),
+						.inpos_14(test_inpos14),
+						.inpos_15(test_inpos15),
+						.inpos_16(test_inpos16),
+						.inpos_17(test_inpos17),
+						.inpos_18(test_inpos18),
+						.inpos_19(test_inpos19),
+						.inpos_20(test_inpos20),
+						.inpos_21(test_inpos21),
+						.inpos_22(test_inpos22),
+						.inpos_23(test_inpos24),
+						.inpos_24(test_inpos25),
+						.inpos_25(test_inpos25),
+						.inpos_26(test_inpos26),
+						.inpos_27(test_inpos27),
+						.inpos_28(test_inpos28),
+						.inpos_29(test_inpos29),
+						.inpos_30(test_inpos30),
+						.inpos_31(test_inpos31),
+						.inpos_32(test_inpos32),
+						.inpos_33(test_inpos33),
+						.inpos_34(test_inpos34),
+						.inpos_35(test_inpos35),
+						.inpos_36(test_inpos36),
+						.inpos_37(test_inpos37),
+						.inpos_38(test_inpos38),
+						.inpos_39(test_inpos39),
+						.inpos_40(test_inpos40),
+						.inpos_41(test_inpos41),
+						.inpos_42(test_inpos42),
+						.inpos_43(test_inpos43),
+						.inpos_44(test_inpos44),
+						.inpos_45(test_inpos45),
+						.inpos_46(test_inpos46),
+						.inpos_47(test_inpos47),
+						.inpos_48(test_inpos48),
+						.inpos_49(test_inpos49),
+						.inpos_50(test_inpos50),
+						.inpos_51(test_inpos51),
+						.inpos_52(test_inpos52),
+						.inpos_53(test_inpos53),
+						.inpos_54(test_inpos54),
+						.inpos_55(test_inpos55),
+						.inpos_56(test_inpos56),
+						.inpos_57(test_inpos57),
+						.inpos_58(test_inpos58),
+						.inpos_59(test_inpos59),
+						.inpos_60(test_inpos60),
+						.inpos_61(test_inpos61),
+						.inpos_62(test_inpos62),
+						.inpos_63(test_inpos63),
+						.inpos_64(test_inpos64),
 						//Output
-						.out(test_out));
+						.out(test_out),
+						.out_pos(test_outpos));
 	initial begin
 		test_in1 = 6'b 100000;
 		test_in2 = 6'b 000000;
@@ -198,8 +329,73 @@ module tb_Arb_complete;
 		test_in62 = 6'b 000000;
 		test_in63 = 6'b 000000;
 		test_in64= 6'b 000000;
+		//Test inpos1
+		test_inpos1 = 6'b 100001;
+		test_inpos2 = 6'b 000000;
+		test_inpos3 = 6'b 000000;
+		test_inpos4 = 6'b 000000;
+		test_inpos5 = 6'b 000000;
+		test_inpos6 = 6'b 000000;
+		test_inpos7 = 6'b 000000;
+		test_inpos8 = 6'b 000000;
+		test_inpos9 = 6'b 000000;
+		test_inpos10 = 6'b 000000;
+		test_inpos11 = 6'b 000000;
+		test_inpos12= 6'b 000000;
+		test_inpos13 = 6'b 000000;
+		test_inpos14 = 6'b 000000;
+		test_inpos15 = 6'b 000000;
+		test_inpos16= 6'b 000000;
+		test_inpos17 = 6'b 000000;
+		test_inpos18 = 6'b 000000;
+		test_inpos19 = 6'b 000000;
+		test_inpos20 = 6'b 000000;
+		test_inpos21 = 6'b 000000;
+		test_inpos22 = 6'b 000000;
+		test_inpos23 = 6'b 000000;
+		test_inpos24= 6'b 000000;
+		test_inpos25 = 6'b 000000;
+		test_inpos26 = 6'b 000000;
+		test_inpos27 = 6'b 000000;
+		test_inpos28 = 6'b 000000;
+		test_inpos29 = 6'b 000000;
+		test_inpos30 = 6'b 000000;
+		test_inpos31 = 6'b 000000;
+		test_inpos32 = 6'b 000000;
+		test_inpos33 = 6'b 000000;
+		test_inpos34 = 6'b 000000;
+		test_inpos35 = 6'b 000000;
+		test_inpos36= 6'b 000000;
+		test_inpos37 = 6'b 000000;
+		test_inpos38 = 6'b 000000;
+		test_inpos39 = 6'b 000000;
+		test_inpos40= 6'b 000000;
+		test_inpos41 = 6'b 000000;
+		test_inpos42 = 6'b 000000;
+		test_inpos43 = 6'b 000000;
+		test_inpos44 = 6'b 000000;
+		test_inpos45 = 6'b 000000;
+		test_inpos46 = 6'b 000000;
+		test_inpos47 = 6'b 000000;
+		test_inpos48= 6'b 000000;
+		test_inpos49 = 6'b 000000;
+		test_inpos50 = 6'b 000000;
+		test_inpos51 = 6'b 000000;
+		test_inpos52= 6'b 000000;
+		test_inpos53 = 6'b 000000;
+		test_inpos54 = 6'b 000000;
+		test_inpos55 = 6'b 000000;
+		test_inpos56 = 6'b 000000;
+		test_inpos57= 6'b 000000;
+		test_inpos58 = 6'b 000000;
+		test_inpos59 = 6'b 000000;
+		test_inpos60= 6'b 000000;
+		test_inpos61 = 6'b 000000;
+		test_inpos62 = 6'b 000000;
+		test_inpos63 = 6'b 000000;
+		test_inpos64= 6'b 000000;
 		#20
-      $display("First 4 Inputs are: %b %b %b %b,Output of Arbiter is:%b",test_in1,test_in2,test_in3,test_in4,test_out);
+      $display("First 4 Inputs are: %b %b %b %b,Output: %b Outputpos: %b",test_in1,test_in2,test_in3,test_in4,test_out,test_outpos);
 		  
 		  
 		  end
