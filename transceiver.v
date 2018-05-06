35253ab795997c129d7a394b4bcdc495efc642c6
Delvin Huynh <delhuynh@ucdavis.edu>
2018-05-06 00:38:51 -0700

    cleaned up transceiver code

--- transceiver.v
+++ transceiver.v
@@ -1,81 +1,68 @@
-module transceiver(
-clk, engineColor, pieceReg, posReg,
-
-U_in,D_in,L_in,R_in,UL_in,UR_in,DL_in,DR_in, 
-UUL_in,UUR_in,LLU_in,RRU_in,DDL_in,DDR_in,LLD_in,RRD_in,
-
-U_out,D_out,L_out,R_out,UL_out,UR_out,DL_out,DR_out, 
-UUL_out,UUR_out,LLU_out,RRU_out,DDL_out,DDR_out,LLD_out,RRD_out,
-
-U_move,D_move,L_move,R_move,UL_move,UR_move,DL_move,DR_move, 
-UUL_move,UUR_move,LLU_move,RRU_move,DDL_move,DDR_move,LLD_move,RRD_move
-
-);
-								
+module transceiver(						
 								
 //CLOCK
-input clk;
-input engineColor;
-input[5:0] pieceReg;
-input[5:0] posReg;
+input clk,
+input engineColor,
+input[5:0] pieceReg,
+input[5:0] posReg,
 //Input Reg
-input [10:0] U_in;
-input [10:0] D_in ;
-input [10:0] L_in;
-input [10:0] R_in;
-input [10:0] UL_in;
-input [10:0] UR_in ;
-input [10:0] DL_in;
-input [10:0] DR_in;
+input [10:0] U_in,
+input [10:0] D_in,
+input [10:0] L_in,
+input [10:0] R_in,
+input [10:0] UL_in,
+input [10:0] UR_in,
+input [10:0] DL_in,
+input [10:0] DR_in,
 //Input knight
-input [7:0] UUL_in;
-input [7:0] UUR_in;
-input [7:0] LLU_in;
-input [7:0] RRU_in;
-input [7:0] DDL_in;
-input [7:0] DDR_in;
-input [7:0] LLD_in;
-input [7:0] RRD_in;
+input [7:0] UUL_in,
+input [7:0] UUR_in,
+input [7:0] LLU_in,
+input [7:0] RRU_in,
+input [7:0] DDL_in,
+input [7:0] DDR_in,
+input [7:0] LLD_in,
+input [7:0] RRD_in,
 
 //Output Non knight
-output reg [10:0] U_out;
-output reg [10:0] D_out;
-output reg [10:0] L_out;
-output reg [10:0] R_out;
-output reg [10:0] UL_out;
-output reg [10:0] UR_out;
-output reg [10:0] DL_out;
-output reg [10:0] DR_out;
+output reg [10:0] U_out,
+output reg [10:0] D_out,
+output reg [10:0] L_out,
+output reg [10:0] R_out,
+output reg [10:0] UL_out,
+output reg [10:0] UR_out,
+output reg [10:0] DL_out,
+output reg [10:0] DR_out,
 //Output knight
-output [7:0] UUL_out;
-output [7:0] UUR_out;
-output [7:0] LLU_out;
-output [7:0] RRU_out;
-output [7:0] DDL_out;
-output [7:0] DDR_out;
-output [7:0] LLD_out;
-output [7:0] RRD_out;
+output [7:0] UUL_out,
+output [7:0] UUR_out,
+output [7:0] LLU_out,
+output [7:0] RRU_out,
+output [7:0] DDL_out,
+output [7:0] DDR_out,
+output [7:0] LLD_out,
+output [7:0] RRD_out,
 
 
 //Output Non knight
-output reg [10:0] U_move;
-output reg [10:0] D_move;
-output reg [10:0] L_move;
-output reg [10:0] R_move;
-output reg [10:0] UL_move;
-output reg [10:0] UR_move;
-output reg [10:0] DL_move;
-output reg [10:0] DR_move;
+output reg [10:0] U_move,
+output reg [10:0] D_move,
+output reg [10:0] L_move,
+output reg [10:0] R_move,
+output reg [10:0] UL_move,
+output reg [10:0] UR_move,
+output reg [10:0] DL_move,
+output reg [10:0] DR_move,
 //Output knight
-output reg [7:0] UUL_move;
-output reg [7:0] UUR_move;
-output reg [7:0] LLU_move;
-output reg [7:0] RRU_move;
-output reg [7:0] DDL_move;
-output reg [7:0] DDR_move;
-output reg [7:0] LLD_move;
-output reg [7:0] RRD_move;
-
+output reg [7:0] UUL_move,
+output reg [7:0] UUR_move,
+output reg [7:0] LLU_move,
+output reg [7:0] RRU_move,
+output reg [7:0] DDL_move,
+output reg [7:0] DDR_move,
+output reg [7:0] LLD_move,
+output reg [7:0] RRD_move
+);
 
 wire [10:0] U_trans;
 wire [10:0] D_trans;
@@ -120,11 +107,13 @@ parameter EMPTY_PIECE_REG = 6'b00_0000;
 
 Transmitter Trans(
 
-engineColor, pieceReg, posReg,
+.engine_color(engineColor), 
+.piece_reg(pieceReg), 
+.pos_reg(posReg),
 
 //out
-U_trans,D_trans,L_trans,R_trans,UL_trans,UR_trans,DL_trans,DR_trans, 
-UUL_trans,UUR_trans,LLU_trans,RRU_trans,DDL_trans,DDR_trans,LLD_trans,RRD_trans
+.U(U_trans), .D(D_trans), .L(L_trans), .R(R_trans), .UL(UL_trans), .UR(UR_trans), .DL(DL_trans), .DR(DR_trans), 
+.UUL(UUL_trans), .UUR(UUR_trans), .LLU(LLU_trans), .RRU(RRU_trans), .DDL(DDL_trans), .DDR(DDR_trans), .LLD(LLD_trans), .RRD(RRD_trans)
 
 );
 
