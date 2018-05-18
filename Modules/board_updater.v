b30055258d71ff60437895dab4662ff74302fd2f
Henry Tran <hhntran@ucdavis.edu>
2018-05-17 20:40:49 -0700

    normal capture

--- Modules/board_updater.v
+++ Modules/board_updater.v
@@ -222,6 +222,7 @@ else if (!init)	begin //Update board state
 	if( !captured_PieceReg && !castling &&!undo) begin // Normal Quiet
 		//Only the corresponding enable bit will be sent, so we dont
 		//care about the initial position
+		//Initial position Piece reg gets set to 0 
 	
 		case (movedPosition)
 		64'd1 :
@@ -350,6 +351,8 @@ else if (!init)	begin //Update board state
 			pieceReg61_out = moved_PieceReg;
 		64'd4611686018427387904 :
 			pieceReg62_out = moved_PieceReg;
+		64'd9223372036854775808:
+			pieceReg63_out = moved_PieceReg;	
 		
 		end case
 	
@@ -357,6 +360,139 @@ else if (!init)	begin //Update board state
 	
 	if( captured_PieceReg && !castling &&!undo) begin // Normal Capture
 	
+	//Forward capture, don't care about initial position
+	//that gets set to 0
+		case (movedPosition)
+		64'd1 :
+			pieceReg0_out = moved_PieceReg;
+		64'd2 :
+			pieceReg1_out = moved_PieceReg;
+		64'd4 :
+			pieceReg2_out = moved_PieceReg;
+		64'd8 :
+			pieceReg3_out = moved_PieceReg;
+		64'd16 :
+			pieceReg4_out = moved_PieceReg;
+		64'd32 :
+			pieceReg5_out = moved_PieceReg;
+		64'd64 :
+			pieceReg6_out = moved_PieceReg;
+		64'd128 :
+			pieceReg7_out = moved_PieceReg;
+		64'd256 :
+			pieceReg8_out = moved_PieceReg;
+		64'd512 :
+			pieceReg9_out = moved_PieceReg;
+		64'd1024 :
+			pieceReg10_out = moved_PieceReg;
+		64'd2048 :
+			pieceReg11_out = moved_PieceReg;
+		64'd4096 :
+			pieceReg12_out = moved_PieceReg;
+		64'd8192 :
+			pieceReg13_out = moved_PieceReg;
+		64'd16384 :
+			pieceReg14_out = moved_PieceReg;
+		64'd32768 :
+			pieceReg15_out = moved_PieceReg;
+		64'd65536 :
+			pieceReg16_out = moved_PieceReg;
+		64'd131072 :
+			pieceReg17_out = moved_PieceReg;
+		64'd262144 :
+			pieceReg18_out = moved_PieceReg;
+		64'd524288 :
+			pieceReg19_out = moved_PieceReg;
+		64'd1048576 :
+			pieceReg20_out = moved_PieceReg;
+		64'd2097152 :
+			pieceReg21_out = moved_PieceReg;
+		64'd4194304 :
+			pieceReg22_out = moved_PieceReg;
+		64'd8388608 :
+			pieceReg23_out = moved_PieceReg;
+		64'd16777216 :
+			pieceReg24_out = moved_PieceReg;
+		64'd33554432 :
+			pieceReg25_out = moved_PieceReg;
+		64'd67108864 :
+			pieceReg26_out = moved_PieceReg;
+		64'd134217728 :
+			pieceReg27_out = moved_PieceReg;
+		64'd268435456 :
+			pieceReg28_out = moved_PieceReg;
+		64'd536870912 :
+			pieceReg29_out = moved_PieceReg;
+		64'd1073741824 :
+			pieceReg30_out = moved_PieceReg;
+		64'd2147483648 :
+			pieceReg31_out = moved_PieceReg;
+		64'd4294967296 :
+			pieceReg32_out = moved_PieceReg;
+		64'd8589934592 :
+			pieceReg33_out = moved_PieceReg;
+		64'd17179869184 :
+			pieceReg34_out = moved_PieceReg;
+		64'd34359738368 :
+			pieceReg35_out = moved_PieceReg;
+		64'd68719476736 :
+			pieceReg36_out = moved_PieceReg;
+		64'd137438953472 :
+			pieceReg37_out = moved_PieceReg;
+		64'd274877906944 :
+			pieceReg38_out = moved_PieceReg;
+		64'd549755813888 :
+			pieceReg39_out = moved_PieceReg;
+		64'd1099511627776 :
+			pieceReg40_out = moved_PieceReg;
+		64'd2199023255552 :
+			pieceReg41_out = moved_PieceReg;
+		64'd4398046511104 :
+			pieceReg42_out = moved_PieceReg;
+		64'd8796093022208 :
+			pieceReg43_out = moved_PieceReg;
+		64'd17592186044416 :
+			pieceReg44_out = moved_PieceReg;
+		64'd35184372088832 :
+			pieceReg45_out = moved_PieceReg;
+		64'd70368744177664 :
+			pieceReg46_out = moved_PieceReg;
+		64'd140737488355328 :
+			pieceReg47_out = moved_PieceReg;
+		64'd281474976710656 :
+			pieceReg48_out = moved_PieceReg;
+		64'd562949953421312 :
+			pieceReg49_out = moved_PieceReg;
+		64'd1125899906842624 :
+			pieceReg50_out = moved_PieceReg;
+		64'd2251799813685248 :
+			pieceReg51_out = moved_PieceReg;
+		64'd4503599627370496 :
+			pieceReg52_out = moved_PieceReg;
+		64'd9007199254740992 :
+			pieceReg53_out = moved_PieceReg;
+		64'd18014398509481984 :
+			pieceReg54_out = moved_PieceReg;
+		64'd36028797018963968 :
+			pieceReg55_out = moved_PieceReg;
+		64'd72057594037927936 :
+			pieceReg56_out = moved_PieceReg;
+		64'd144115188075855872 :
+			pieceReg57_out = moved_PieceReg;
+		64'd288230376151711744 :
+			pieceReg58_out = moved_PieceReg;
+		64'd576460752303423488 :
+			pieceReg59_out = moved_PieceReg;
+		64'd1152921504606846976 :
+			pieceReg60_out = moved_PieceReg;
+		64'd2305843009213693952 :
+			pieceReg61_out = moved_PieceReg;
+		64'd4611686018427387904 :
+			pieceReg62_out = moved_PieceReg;
+		64'd9223372036854775808:
+			pieceReg63_out = moved_PieceReg;	
+		
+		end case
 	
 	end
 	
