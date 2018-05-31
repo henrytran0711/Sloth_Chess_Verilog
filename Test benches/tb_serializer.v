b98a17872509c68486ed2120790b7cb264fb7090
Henry Tran <hhntran@ucdavis.edu>
2018-05-30 21:55:19 -0700

    serializer works

--- Test benches/tb_serializer.v
+++ Test benches/tb_serializer.v
@@ -2,12 +2,10 @@ module serializer_tb();
   
 reg clock = 0;
 reg load = 0;
-reg [31:0] in;
+reg [24191:0] in;
+reg w_en;
 
-wire [3:0] out1;
-wire [3:0] out2;
-wire [3:0] out3;
-wire [3:0] out4;
+wire [31:0] out1;
 wire [3:0] move_counter_out;
 wire done;
 	
@@ -16,30 +14,28 @@ wire done;
     clock = ~clock;
   end
   
-  serializer sr(.clk(clock),.load(load),.in(in),
-  .out1(out1),.out2(out2),.out3(out3),.out4(out4),
-  .move_counter_out(move_counter_out),.done(done));
   
+  serializer sr(.clk(clock), .in(in), .load(load),
+  .w_en(w_en), .data_out(out1), .move_counter_out(move_counter_out), .done(done));
    initial begin
    
-     in = 32'b00010010001101000101011001110000;
-	 //0001 0010 0011 0100 || 0101 0110 0111 0000
+     in = 24192'd000011111111111111111111111114294967296;
+	//All 0s, several highs
 	 load = 1;
      
        
        #20;
 	 load = 0;
-	 $display("in = %b", in);
-	 $display("out1 = %b, out2 = %b, out3 = %b, out4 = %b", out1, out2, out3, out4); 
+	 w_en = 1;
+	
+	 $display("out[31:0] = %b", out1); 
 	 $display("move counter = %b, done = %b", move_counter_out, done); 
-      #200;
-     $display("in = %b", in);
-	 $display("out1 = %b, out2 = %b, out3 = %b, out4 = %b", out1, out2, out3, out4); 
-	 $display("move counter = %b, done = %b",move_counter_out, done);     
-        #200;
-     $display("in = %b", in);
-	 $display("out1 = %b, out2 = %b, out3 = %b, out4 = %b", out1, out2, out3, out4); 
-	 $display("move counter = %b, done = %b",move_counter_out, done);     
+      #2000;
+      	 $display("out[31:0] = %b", out1); 
+	 $display("move counter = %b, done = %b", move_counter_out, done);
+        #15000;
+    	 $display("out[31:0] = %b", out1); 
+	 $display("move counter = %b, done = %b", move_counter_out, done);
      
      
    #10 $finish;
